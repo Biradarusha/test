3a5b98c3366494b094bef4471387a0b916753022
Biradarusha <usha.biradar009@gmail.com>
2024-08-16 17:33:11 +0530

    new

---
+++ first.js
@@ -0,0 +1,17 @@
+//Data Types :-
+// Primitives Data Types
+
+// 1. strings
+var a = 30;
+
+//2. number
+var uname = "Usha";
+
+//3. boolean
+var b1 = true;
+
+//4. Null
+b;
+
+//undefined
+var u;
---
+++ hello.py
@@ -0,0 +1,49 @@
+a=2
+b=3
+c=0
+
+c=a
+a=b
+b=c
+print(a,b)
+
+
+"""a=a+b
+b=a-b
+a=a-b
+print(a,b) """
+
+a=5
+b=3
+c=a+b
+print(c)
+
+"""  a,b=b,a
+print(a,b)"""
+
+"""d=a/b
+print(a/b)
+
+e=a%b
+print(e)
+
+f=a//b
+print(f)   """
+
+"""if a>b:
+    print("a is greater")
+else:
+    print("b is greater")"""
+
+x=2
+
+if(x%2==0):
+        print( x,"is even no")
+else:
+        print( x,"is odd number")
+# f string
+y=3
+if(y%2==0):
+        print( f"{y} is even no")
+else:
+        print( f"{y} is odd number")
